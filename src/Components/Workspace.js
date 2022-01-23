b22b070488576c6eadd449d20e62db807396df5f
Radi-dev <evaradi18@gmail.com>
2022-01-23 15:08:26 +0100

    edit draggable

--- src/Components/Workspace.js
+++ src/Components/Workspace.js
@@ -2,14 +2,13 @@ import React from "react";
 import Draggable from "react-draggable";
 const Workspace = React.forwardRef(({ image, ...props }, ref) => {
   return (
-    <section ref={ref} className="relative w-full h-full border m-4">
+    <section ref={ref} className="relative w-full h-full border">
       {props.children.map((child, i) => (
         <Draggable key={i} bounds="parent">
           <div className="absolute">{child}</div>
         </Draggable>
       ))}
       <img src={image} alt="" className="mb-4"></img>
-      <div>HELLO</div>
     </section>
   );
 });
