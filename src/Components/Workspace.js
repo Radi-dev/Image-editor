9021bd59f56b3f6fdd3d19570b8950707775dd14
Radi-dev <evaradi18@gmail.com>
2022-01-23 14:52:20 +0100

    add draggable

--- src/Components/Workspace.js
+++ src/Components/Workspace.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Draggable from "react-draggable";
 const Workspace = React.forwardRef(({ image, ...props }, ref) => {
   return (
     <section ref={ref} className="relative">
       {props.children.map((child, i) => (
-        <div key={i} className="w-full h-full">
-          {child}
-        </div>
+        <Draggable key={i}>
+          <div className="w-full h-full">{child}</div>
+        </Draggable>
       ))}
       <img src={image} alt="" className="mb-4"></img>
       <div>HELLO</div>
